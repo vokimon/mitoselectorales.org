8b590d71bdb959a7aaa8df8ef800c64294302c4c
David García Garzón <voki@canvoki.net>
2019-02-19 10:11:18 +0100

    final domain in publishconf

--- publishconf.py
+++ publishconf.py
@@ -12,6 +12,7 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://vokimon.github.io/mitoselectorales.org'
+SITEURL = 'https://votomitico.org'
 RELATIVE_URLS = True
 
 PLUGINS = PLUGINS + [
